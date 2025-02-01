0ebc14d1a057f0468b7b34cafd8e6b95085fcdfc
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-01 13:34:55 -0500

    Script updated and compiler warnings resolved

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,27 +2,28 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {AMM} from "../src/Counter.sol";
+import {AMMReference} from "../src/AMMReference.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SERC20} from "../src/SERC20.sol";
 
-contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol, decimals) {}
+contract MockERC20 is SERC20 {
+    constructor(string memory name, string memory symbol) SERC20(name, symbol) {}
 }
 
 contract CounterScript is Script {
-    AMM public amm;
+    AMMReference public amm;
     MockERC20 public baseAsset;
     MockERC20 public quoteAsset;
 
     function setUp() public {
-        baseAsset = new MockERC20("Base Asset", "BASE", 18);
-        quoteAsset = new MockERC20("Quote Asset", "QUOTE", 18);
+        baseAsset = new MockERC20("Base Asset", "BASE");
+        quoteAsset = new MockERC20("Quote Asset", "QUOTE");
     }
 
     function run() public {
         vm.startBroadcast();
 
-        amm = new AMM(baseAsset, quoteAsset, 1e18, 1e18);
+        amm = new AMMReference(baseAsset, quoteAsset, 1e18, 1e18);
 
         vm.stopBroadcast();
     }
