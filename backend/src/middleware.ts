b552a1e48c28c7cbe65cd1e8f384f544080a2aa6
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-17 17:04:29 -0500

    authMiddleware logic added and user.ts modified

---
+++ backend/src/middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.get('authorization') ?? "";
+
+    try {
+        const decodedjwt = jwt.verify(authHeader, JWT_SECRET);
+        console.log(decodedjwt);
+
+        if (decodedjwt.userId) {
+            req.userId = decodedjwt.userId;
+            next();
+        } else {
+            return res.status(403).json({
+                message: "Error while logging in"
+            })
+        }
+    } catch(e) {
+        console.log("You're not logged in");
+        return res.status(403).json({
+            message: "You're not logged in"
+        });
+    }
+}
