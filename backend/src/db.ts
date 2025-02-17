e8479a220314a5767319ebe7ddd37f26099f682e
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-17 16:25:04 -0500

    index.ts updated with router

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prismaClient = new PrismaClient();
 
 export const getNextTrade = async (userId: number) => {
-    const trade = await prismaClient.task.findFirst({
+    const trade = await prismaClient.trade.findFirst({
         where: {
             
         }
