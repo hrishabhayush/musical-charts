1360d723ff527cb805ac0e2c8615f73be71b28b3
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-03-30 19:32:37 -0400

    createRiffToken function created along with test case

--- src/RiffFactory.sol
+++ src/RiffFactory.sol
@@ -2,20 +2,52 @@
 pragma solidity ^0.8.13;
 
 import "./ViolinCoin.sol";
+import "./Riff.sol";
 
 contract RiffFactory {
+
+    struct riffToken {
+        string name;
+        string ticker;
+        string tokenImageUrl;
+        uint256 fundingRaised;
+        address tokenAddress;
+        address creatorAddress;
+    }
+
+    mapping(address => riffToken) public addressToRiffTokenMapping;
 
     uint constant DECIMALS = 10 * 18;
     uint256 constant MAX_SUPPLY = 1000000 * DECIMALS;
     uint256 constant INIT_SUPPLY = 20 * MAX_SUPPLY / 100;
 
-    saddress[] tokens;
+    uint256 constant RIFFTOKEN_FUNDING_GOAL = 24 ether;
+
+    uint256 constant RIFFTOKEN_CREATION_FEE = 0.0001 ether;
+
+    address[] tokens;
     suint256 tokenCount;
 
-    function createRiffToken(string memory name, string memory symbol, string memory imageUrl) private returns(saddress) {
+    /*
+     * Create a riff token with name, ticker, and image. 
+     */
+    function createRiffToken(string memory name, string memory ticker, string memory imageUrl) public payable returns(address) {
+        require(msg.value >= RIFFTOKEN_CREATION_FEE, "Invalid token creation fee");
 
-        ViolinCoin violinCoin = new ViolinCoin(msg.sender, name, symbol, uint8(DECIMALS));
-        saddress violinCoinAddress = saddress(violinCoin);
+        ViolinCoin violinCoin = new ViolinCoin(msg.sender, name, ticker, uint8(DECIMALS));
+        address violinCoinAddress = address(violinCoin);
+        riffToken memory newToken = riffToken(name, ticker, imageUrl, 0, violinCoinAddress, msg.sender);
+        addressToRiffTokenMapping[violinCoinAddress] = newToken;
         return violinCoinAddress;
     }
+
+    function buyRiffToken(address riffTokenAddress, suint256 quantity) public payable returns (uint) {
+        require(addressToRiffTokenMapping[riffTokenAddress].tokenAddress!=address(0), "Token is not listed on the platform");
+
+        // Find the token  
+        riffToken storage listedToken = addressToRiffTokenMapping[riffTokenAddress];
+
+        require(addressToRiffTokenMapping[riffTokenAddress].fundingRaised >= RIFFTOKEN_FUNDING_GOAL, "Token has not reached the funding goal yet");
+    }
+
 }
